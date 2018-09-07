bbdefd132cbc70efe04f1a252addf69aa750bfdc
tinysakura <2796191719@qq.com>
2018-09-07 23:53:11 +0800

    eureka消费者构建完成

---
+++ src/main/java/com/cfh/eurekaconsumer/EurekaConsumerApplication.java
@@ -0,0 +1,24 @@
+package com.cfh.eurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+//获得服务发现能力
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConsumerApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced//获得客户端的负载均衡能力
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+}
